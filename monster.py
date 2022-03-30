d2b5ed2b056625236506862ff7e145e746721831
Znarfen <97677302+Znarfen@users.noreply.github.com>
2022-03-30 10:07:20 +0200

    moves
    do is cool

--- monster.py
+++ monster.py
@@ -1,25 +1,12 @@
 import moveset
 
-ph = 10
-oh = 10
-while ph > 0 and oh > 0:
 
-    print("player health:", ph, "\noponent health:", oh, " \n\n")
-    at = int(input(" 1, Punch \n 2, Wip \n\n:"))
+while True:
 
-    if at == 1:
-        at = "Punch"
-    if at == 2:
-        at = "Wip"
+    at = str(input(":"))
 
-    at = moveset.Do(at).attack()
+    at = moveset.AirType(at).do()
     print(at)
-    oh = oh - at
-
-    print("\n\nÄggis use Punch\n\n")
-
-    x = moveset.Do("Punch").attack()
-    ph = ph - x
 
 
 
--- moveset.py
+++ moveset.py
@@ -1,29 +1,50 @@
-
+import random
 
 class Attack:
-    def __init__(self, at = 0, xp = 1):
+    def __init__(self, at, xp = 1):
         self.at = at
         self.xp = xp
 
-    def normal(self):
+    def do(self):
 
-        if self.at == 0:
-            return(0 * self.xp)
+        if self.at == "wipp":
+            r = random.randint(30,70)
+            return(int(r * self.xp))
 
-        if self.at == 1:
-            return(float(1 * self.xp))
+        if self.at == "punch":
+            r = random.randint(30,70)
+            return(int(r * self.xp))
 
-        if self.at == 2:
-            return(float(3 * self.xp))
+#Normal moves: wip, punch
 
-    
-class Fly(Attack):
+class AirType(Attack):
     def __init__(self, at=0, xp=1):
         super().__init__(at, xp)
 
+    def do(self):
+        if self.at == "fly":
+            r = random.randint(40,60)
+            return(int(r * self.xp))
+        return super().do()
+
+#Air moves: fly
+
+class FireType(Attack):
+    def __init__(self, at=0, xp=1):
+        super().__init__(at, xp)
+
+    def do(self):
+        if self.at == "burn":
+            r = random.randint(20,80)
+            return(int(r * self.xp))
+        return super().do()
+
+#Fire moves: burn
 
 
-#class Ground(Attack):
+
+    
+
 
 
         
