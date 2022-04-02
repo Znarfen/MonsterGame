79ad5ce53f75afc781d6e7b9bf59980f0a3ec19e
Znarfen <97677302+Znarfen@users.noreply.github.com>
2022-04-02 09:43:24 +0200

    thing

--- class.py
+++ class.py
@@ -1,7 +1,7 @@
 import random
 
 class Normal:
-    def __init__(self, at, xp = 1):
+    def __init__(self, at = "", xp = 1):
         self.at = at
         self.xp = xp
 
@@ -19,11 +19,14 @@ class Normal:
             r = random.randint(45, 55)
             return(int(r * self.xp))
 
-#Normal moves: wip, punch
+    def allmoves(self):
+        move = ["wipp", "punch", "cut"]
+        return(move)
 
+#Normal moves: wip, punch, cut
 
 class AirType(Normal):
-    def __init__(self, at, xp = 1):
+    def __init__(self, at = "", xp = 1):
         super().__init__(at, xp)
 
     def do(self):
@@ -32,10 +35,14 @@ class AirType(Normal):
             return(int(r * self.xp))
         return super().do()
 
+    def allmoves(self):
+        move = ["fly"] + super().allmoves()
+        return(move)
+
 #Air moves: fly
 
 class FireType(Normal):
-    def __init__(self, at, xp = 1):
+    def __init__(self, at = "", xp = 1):
         super().__init__(at, xp)
 
     def do(self):
@@ -43,6 +50,10 @@ class FireType(Normal):
             r = random.randint(20,80)
             return(int(r * self.xp))
         return super().do()
+
+    def allmoves(self):
+        move = ["burn"] + super().allmoves()
+        return(move)
 
 #Fire moves: burn
 
--- monster.py
+++ monster.py
@@ -1,12 +1,49 @@
 import moveset
+import random
 
+ph = 1000
+oh = 1000
+
+mo = moveset.AirType().allmoves()
+x = random.choice(mo)
+mo.remove(x)
+y =random.choice(mo)
+
+mo = ["burn", "punch"]
 
 while True:
+    if oh < 0:
+        print("Player won")
+        break
+    if ph < 0:
+        print("Oponent won")
+        break
 
-    at = str(input(":"))
+    print("player:", ph)
+    print("oponent", oh)
+    print()
 
-    at = moveset.AirType(at).do()
-    print(at)
+    z = input("1)" + x + "\n2)" + y +"\n \n:")
+    if z == "1":
+        z = x
+    if z == "2":
+        z = y
+
+    at = moveset.AirType(z).do()
+    oh = oh - at
+    print("Player:", z)
+    print(at, "was made by the player\n")
+
+    r = random.choice(mo)
+    
+    at = moveset.FireType(r).do()
+    ph = ph - at
+
+    print("Oponent:", r,)
+    print(at, "was made by the oponent\n")
+
+
+
 
 
 
