a3892cae85bb251795cfdf2619c23a610c7b7cdb
Znarfen <97677302+Znarfen@users.noreply.github.com>
2022-04-21 15:08:22 +0200

    New Classes

--- monsters.py
+++ monsters.py
@@ -1,4 +1,18 @@
+from mimetypes import init
 import monstertypes
 
 
-class Monsters()
+class Rat():
+    def __init__(self, at = 1, hp = 1):
+        self.at = at
+        self.hp = hp
+
+    def attack(self):
+        return(self.at)
+
+class FlygFan(Rat):
+    def __init__(self, at=3, hp=1):
+        super().__init__(at, hp)
+
+
+print(FlygFan().attack())
