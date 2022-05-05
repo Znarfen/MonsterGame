9b78399237b9246f007a4f2a3ac9177f514e4de1
Znarfen <97677302+Znarfen@users.noreply.github.com>
2022-05-05 15:08:03 +0200

    new

--- MonsterGame.py
+++ MonsterGame.py
@@ -1,9 +1,38 @@
-from urllib import response
-import requests
-
-response1 = requests.get("https://random-word-api.herokuapp.com/word").json()[0]
-response2 = requests.get("https://random-word-api.herokuapp.com/word").json()[0]
-name = response1 + "-" + response2
+import monsters
 
 
-print(name)
+level = 0
+
+mon1 = monsters.Generator().new()
+mon2 = monsters.Generator().new()
+mon3 = monsters.Generator().new()
+
+def about(mon):
+    n1 = ("name = " + mon.name())
+    n2 = ("power = " + str(mon.power()))
+    n3 = ("hp = " + str(mon.hp()))
+    n4 = ("type = " + mon.type())
+    print(n1, "/", n2, "/", n3, "/", n4)
+    print()
+
+deck = [mon1, mon2, mon3]
+
+while True:
+    level += 1
+
+    ii = 1
+    print("This is the deck you have:")
+    for i in deck:
+        print("Monster-" + str(ii) + ":")
+        about(i)
+        ii += 1
+    print("")
+
+    opponent = monsters.Generator().new()
+    print("About your opponent:")
+    about(opponent)
+
+    choice = int(input(":"))
+    
+
+    
--- monsters.py
+++ monsters.py
@@ -8,7 +8,7 @@ class NoType():
         self.monstername = monstername
         self.monstertype = monstertype
 
-    def attack(self):
+    def power(self):
         return(self.at)
 
     def name(self):
@@ -41,25 +41,25 @@ class Battle():
 
     def attack(self):
         if self.mon1.type() == self.mon2.type():
-            ret = (self.mon1.attack())
+            ret = (self.mon1.power())
 
         if self.mon1.type() == "Fire":
             if self.mon2.type() == "Grass":
-                ret = (self.mon1.attack() + 2)
+                ret = (self.mon1.power() + 2)
             if self.mon2.type() == "Water":
-                return(self.mon1.attack() - 2)
+                return(self.mon1.power() - 2)
 
         if self.mon1.type() == "Water":
             if self.mon2.type() == "Grass":
-                ret = (self.mon1.attack() - 2)
+                ret = (self.mon1.power() - 2)
             if self.mon2.type() == "Fire":
-                ret = (self.mon1.attack() + 2)
+                ret = (self.mon1.power() + 2)
 
         if self.mon1.type() == "Grass":
             if self.mon2.type() == "Water":
-                ret = (self.mon1.attack() + 2)
+                ret = (self.mon1.power() + 2)
             if self.mon2.type() == "Fire":
-                ret = (self.mon1.attack() - 2)
+                ret = (self.mon1.power() - 2)
 
         if ret > 0:
             ret = 0
@@ -70,15 +70,15 @@ class Upgrade():
         self.mon = mon
 
     def hp(self):
-        at = self.mon.attack()
+        at = self.mon.power()
         health = self.mon.hp() + 2
         name = self.mon.name()
         type = self.mon.type()
 
         return(NoType(at, health, name, type))
 
-    def attack(self):
-        at = self.mon.attack() + 2
+    def at(self):
+        at = self.mon.power() + 2
         health = self.mon.hp()
         name = self.mon.name()
         type = self.mon.type()
@@ -86,30 +86,47 @@ class Upgrade():
         return(NoType(at, health, name, type))
 
 class Generator():
-    def new():
+    def __init__(self, level = 1):
+        self.level = level
+
+    def new(self):
         names = []
-        name = ["Ägg", "Jon", "Matt", "Nick, Fregroj", "Nick", "Zac", 
+        listname = ["Ägg", "Jon", "Matt", "Nick, Fregroj", "Nick", "Zac", 
         "No", "Red", "Blue", "Yelow", "Grean", "Demo", "Null", "Gass", 
-        "One", "PeePee", "UwU", "No", "Mon", "OwO", "Lul", "Fast"]
+        "One", "PeePee", "UwU", "No", "Mon", "OwO", "Lul", "Fast", "Bot"]
 
         try:
             name1 = requests.get("https://random-word-api.herokuapp.com/word").json()[0]
-            name2 = random.choice(name)
+            name2 = random.choice(listname)
             
         except:
 
-            name1 = random.choice(name)
-            name2 = random.choice(name)
+            name1 = random.choice(listname)
+            name2 = random.choice(listname)
 
         names.append(name1 + "-" + name2)
         names.append(name2 + "-" + name1)
 
-        return(random.choices(names)[0].upper())
+        g = self.level + 10
+        num = random.randint(1, g)
+        at = num
+        hp = g - num
+        name = (random.choices(names)[0].upper())
 
-        
+        r = random.choice(["Fire", "Water", "Grass"])
 
-print(Generator.new())
+        if r == "Fire":
+            return(Fire(at, hp, name))
 
+        if r == "Water":
+            return(Water(at, hp, name))
+
+        if r == "Grass":
+            return(Grass(at, hp, name))
+
+
+#new = Generator().new()
+#print(new.name(), new.power(), new.hp(), new.type())
 
 
 
