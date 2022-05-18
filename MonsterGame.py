07ca20887341a0d2e07e1ccbccea1dd29367dc5c
Znarfen <97677302+Znarfen@users.noreply.github.com>
2022-05-18 10:01:18 +0200

    Test

--- MonsterGame.py
+++ MonsterGame.py
@@ -5,7 +5,7 @@ import time
 #Print+
 def say(a = "", b = "", c = "", d = ""):
     print(a, b, c, d)
-    time.sleep(0.5)
+    time.sleep(0.2)
 
 def about(mon):
     n1 = ("name = " + mon.name())
@@ -14,6 +14,17 @@ def about(mon):
     n4 = ("type = " + mon.type())
     say(n1 + " / " + n2 + " / " + n3 + " / " + n4)
     say()
+
+def showdeck(deck):
+    say("This is the deck you have:")
+    ii = 1
+    for i in deck:
+        say(str(ii) + ". " + "Monster-" + str(ii) + ":")
+        about(i)
+        ii += 1
+    say("")
+
+
 
 def game():
     mon1 = monsters.Generator(2, "Fire").new()
@@ -27,16 +38,10 @@ def game():
         level += 1
         say("----------------")
         say()
-        ii = 1
+        
+        showdeck(deck)
 
-        say("This is the deck you have:")
-        for i in deck:
-            say("Monster-" + str(ii) + ":")
-            about(i)
-            ii += 1
-        say("")
-
-        opponent = monsters.Generator(level * 4).new()
+        opponent = monsters.Generator(level).new()
         say("About your opponent:")
         about(opponent)
 
@@ -50,35 +55,45 @@ def game():
                 say("Wrong Input\n")
 
         ph = pmon.hp()
-        pa = monsters.Battle(pmon, opponent).attack()
-        
         oh = opponent.hp()
-        oa = monsters.Battle(opponent, pmon).attack()
         
         h = 0
         while True:
+            pa = monsters.Battle(pmon, opponent).attack()
+            oa = monsters.Battle(opponent, pmon).attack()
+
+            rpa = pmon.power()
+            roa = opponent.power()
+
             h += 1
             say("Round " + str(h))
             say()
 
-            r = random.randint(1, 20)
-            if r == 1:
-                say("Player: SUPER-DUPER-DUPER ATTACK")
-                pa = pa + 4
-            
-            r = random.randint(1, 20)
-            if r == 1:
-                say("Opponent: SUPER-DUPER-DUPER ATTACK")
-                oa = oa + 4
+            pb = pa - rpa
+            ob = oa - roa
+
+            if pb >= 0:
+                pb = "+" + str(pb)
+            if ob >= 0:
+                ob = "+" + str(ob)
+
+            pa2 = str(rpa) + " + (" + str(pb) + ")"
+            oa2 = str(roa) + " + (" + str(ob) + ")"
+
+            say("Before:")
+            say("Player attak:", pa2, "/ Player hp:", ph)
+            say("Opponernt attak:", oa2, "/ Opponent hp:", oh)
+            say()
 
             ph = ph - oa
             oh = oh - pa
 
-            say("Player attak:", pa, "/ Player hp:", ph)
-            say("Opponernt attak:", oa, "/ Opponent hp:", oh)
+            say("After:")
+            say("Player attak:", pa2, "/ Player hp:", ph)
+            say("Opponernt attak:", oa2, "/ Opponent hp:", oh)
             say()
-                
-            if oh or ph < 0:
+
+            if (0 >= oh) or (0 >= ph):
 
                 if ph < oh:
                     win = False
@@ -95,7 +110,7 @@ def game():
             say("------------")
             say()
 
-        say("Score: ", level)
+        say("Score:", level)
         if win == True:
             say("You won!")
             input("Press ENTER")
@@ -111,22 +126,16 @@ def game():
             if len(deck) == 0:
                 say("All your monsters has ben defeted!")
                 say()
-
-                #name = input("Enter your name:")
-                #f = open("Score.txt", "a")
-                #f.write(name + "" + str(level) + "\n")
-                #say()
+                input("Press ENTER ")
+                
                 break
+
+        say()
         
         ii = 1
 
         if win == True:
-            say("This is the deck you have:")
-            for i in deck:
-                say("Monster-" + str(ii) + ":")
-                about(i)
-                ii += 1
-            say("")
+            showdeck(deck)
 
             while True:
                 try:
@@ -139,28 +148,28 @@ def game():
                     if r == 0:
                         if choice1 == 1:
                             mon1 = monsters.Upgrade(pmon).at()
-                            deck.append(mon1)
+                            deck.insert(0, mon1)
 
                         if choice1 == 2:
                             mon2 = monsters.Upgrade(pmon).at()
-                            deck.append(mon2)
+                            deck.insert(1, mon2)
 
                         if choice1 == 3:
                             mon3 = monsters.Upgrade(pmon).at()
-                            deck.append(mon3)
+                            deck.insert(2, mon3)
                     
                     if r == 1:
                         if choice1 == 1:
                             mon1 = monsters.Upgrade(pmon).hp()
-                            deck.append(mon1)
+                            deck.insert(0, mon1)
 
                         if choice1 == 2:
                             mon2 = monsters.Upgrade(pmon).hp()
-                            deck.append(mon2)
+                            deck.insert(1, mon2)
 
                         if choice1 == 3:
                             mon3 = monsters.Upgrade(pmon).hp()
-                            deck.append(mon3)
+                            deck.insert(2, mon3)
 
                     break
                 except:
@@ -171,7 +180,6 @@ def main():
         say("----------")
         say("1. PLAY")
         say("2. SETINGS")
-        say("3. HARD MODE")
         say("----------")
         say()
 
